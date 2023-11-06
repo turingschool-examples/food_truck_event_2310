3b092c063dca6b030105a0dae424a658d6338ccf
Sam Puttman <daputter2@gmail.com>
2023-11-06 11:39:34 -0500

    build out food truck class

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -3,6 +3,15 @@ class FoodTruck
 
   def initialize(name)
     @name = name
-    @inventory = {}
+    @inventory = Hash.new(0)
+  end
+
+  #return 0 if item isn't found.
+  def check_stock(item)
+    @inventory[item] || 0
+  end
+
+  def stock(item, quantity)
+    @inventory[item] += quantity
   end
 end

9d22ab91e2ae1c02c04f3a3c31473cdb942c1a1d
Martin Chavez <martinchavez1203@gmail.com>
2023-11-06 09:14:19 -0700

    Item class can read attributes

--- lib/item.rb
+++ lib/item.rb
@@ -2,6 +2,6 @@ class Item
   attr_reader :name, :price
   def initialize(info)
     @name = info[:name]
-    @price = info[:price].delete(",$")
+    @price = info[:price].delete("$").to_f
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -11,4 +11,11 @@ RSpec.describe Item do
       expect(@item1).to be_a(Item)
     end
   end
+
+  describe '#reads attributes' do
+    it 'returns and reads the attributes ' do
+      expect(@item2.name).to eq("Apple Pie (Slice)")
+      expect(@item2.price).to eq(2.50)
+    end
+  end
 end

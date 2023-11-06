339e41fe64901b192dfff445b9d0e540b7afcc20
Martin Chavez <martinchavez1203@gmail.com>
2023-11-06 10:17:30 -0700

    food_truck_names method works with passing test

--- lib/event.rb
+++ lib/event.rb
@@ -4,4 +4,14 @@ class Event
     @name = name
     @food_trucks = []
   end
+
+  def add_food_truck(truck)
+    @food_trucks << truck
+  end
+
+  def food_truck_names
+    @food_trucks.map do |truck|
+      truck.name
+    end
+  end
 end

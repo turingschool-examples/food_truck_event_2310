3e576e71bd6484f41e3cf7c11c2da5e56c0a6e55
Dylan Perry <dylanbperry@gmail.com>
2023-11-06 08:14:48 -0800

    Add attr_reader for name and price values, and convert price from string to float

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -6,4 +6,14 @@ describe Item do
 
         expect(item1).to be_a Item
     end
+
+    it "stores and reads a name and price value" do
+        item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+        expect(item1.name).to eq("Peach Pie (Slice)")
+        expect(item1.price).to eq(3.75)
+        expect(item2.name).to eq("Apple Pie (Slice)")
+        expect(item2.price).to eq(2.50)
+    end
 end

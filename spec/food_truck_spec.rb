5cb4d2b39b34156210836b642f5e871e3a855dc3
Dylan Perry <dylanbperry@gmail.com>
2023-11-06 08:18:49 -0800

    Add attr_reader for @name, and create empty hash for @inventory

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -7,4 +7,11 @@ describe FoodTruck do
 
         expect(food_truck).to be_a FoodTruck
     end
+
+    it "stores and reads a name, and creates default empty inventory hash" do
+        food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+        expect(food_truck.name).to eq("Rocky Mountain Pies")
+        expect(food_truck.inventory).to eq({})
+    end
 end

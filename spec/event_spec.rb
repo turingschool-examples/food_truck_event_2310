f33854fc6ffb7cafc53a5ba6beabafb59a87d56d
Stephen Nash <stephen.aa.nash@gmail.com>
2023-11-06 12:59:00 -0500

    feat&test: Add the add_food_truck method

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -7,17 +7,24 @@ RSpec.describe Event do
   before(:each) do
     @event = Event.new("South Pearl Street Farmers Market")
   end
-
+  
   it 'exists' do
     expect(@event).to be_an_instance_of(Event)
   end
-
+  
   it 'has a name' do
     expect(@event.name).to eq("South Pearl Street Farmers Market")
   end
-
+  
   it 'has no food trucks' do
     expect(@event.food_trucks).to eq([])
+  end
+  
+  it 'can add a food truck' do
+    @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    @event.add_food_truck(@food_truck1)
+
+    expect(@event.food_trucks).to eq([@food_truck1])
   end
 
 end

a2576b4430671774ce88f1aaf0dec1c93b0dd9cc
Nathan Lambertson <lambertones@gmail.com>
2023-11-06 10:14:57 -0600

    Start food truck tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,5 +2,19 @@ require './lib/item'
 require './lib/food_truck'
 
 RSpec.describe Item do
-  describe
+  describe 'initialize' do
+    it 'exists' do
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+      expect(item1).to be_a Item
+    end
+
+    it "has two attributes upon initialize" do
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+      expect(item2.name).to eq("Apple Pie (Slice)")
+      expect(item2.price).to eq(2.50)
+    end
+  end
 end

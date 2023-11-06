6c700e0e31333b5d2dd178891bca79f5d48cddc5
Dylan Perry <dylanbperry@gmail.com>
2023-11-06 08:45:07 -0800

    Add method and tests for add_food_truck

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -16,4 +16,25 @@ describe Event do
         expect(event.name).to eq("South Pearl Street Farmers Market")
         expect(event.food_trucks).to eq([])
     end
+
+    it "adds a food truck" do
+        event = Event.new("South Pearl Street Farmers Market")
+        food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+        food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+
+        item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+        item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+        item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+        item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+        food_truck1.stock(item1, 35)
+        food_truck1.stock(item2, 7)
+        food_truck2.stock(item4, 50)
+        food_truck2.stock(item3, 25)
+
+        event.add_food_truck(food_truck1)
+        event.add_food_truck(food_truck2)
+
+        expect(event.food_trucks).to include(food_truck1, food_truck2)
+    end
 end

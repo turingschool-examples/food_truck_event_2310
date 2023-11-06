2074b76f7b0f4a06097a198195436136344b0033
Dana Howell <howelld115@gmail.com>
2023-11-06 11:27:53 -0700

    test: Adds #add_food_truck test

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -14,8 +14,8 @@ RSpec.describe Event do
     end
   end
 
-  describe 'add food trucks' do
-    it 'Add food trucks with their inventory to event' do
+  describe 'adds items to food trucks' do
+    it 'Add items to food trucks going to the event' do
       @event = Event.new("South Pearl Street Farmers Market")
       @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
       @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")    
@@ -24,7 +24,7 @@ RSpec.describe Event do
       @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
       @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
       @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-
+      
       @food_truck1.stock(@item1, 35)    
       @food_truck1.stock(@item2, 7)  
       @food_truck2.stock(@item4, 50)    
@@ -33,8 +33,23 @@ RSpec.describe Event do
     end
   end
 
-#      @event.add_food_truck(food_truck1)    
-    #  @event.add_food_truck(food_truck2)    
-    #  @event.add_food_truck(food_truck3)
+  describe '#add_food_truck' do
+    it 'Adds food truck to the event' do
+      @event = Event.new("South Pearl Street Farmers Market")
+      @food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      @food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")    
+      @food_truck3 = FoodTruck.new("Palisade Peach Shack")    
+
+      expect(@food_trucks).to eq([])
+
+      @event.add_food_truck(food_truck1)    
+      @event.add_food_truck(food_truck2)    
+      @event.add_food_truck(food_truck3)
+
+      expect(@food_trucks).to eq([@food_truck1, @food_truck2, @food_truck3])
+    end
+  end
+
+     
 end
 

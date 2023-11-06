5be0e201383aa577ed66674641f41c2c84e36b4f
Nathan Lambertson <lambertones@gmail.com>
2023-11-06 10:30:58 -0600

    Finish tests for iteration 1

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -16,4 +16,28 @@ RSpec.describe FoodTruck do
       expect(food_truck.inventory).to eq({})
     end
   end
+
+  describe '#check_stock' do
+    it 'can check to see the amount of an item given' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+      expect(food_truck.check_stock(item1)).to eq(0)
+    end
+  end
+
+  describe '#stock' do
+    it 'can stock an item into the inventory0' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      food_truck.stock(item1, 30)
+      food_truck.stock(item2, 12)
+
+      expect(food_truck.check_stock(item1)).to eq(30)
+      food_truck.stock(item1, 25)
+      expect(food_truck.check_stock(item1)).to eq(55)
+
+      expect(food_truck.inventory.values).to eq([55, 12])
+    end
+  end
 end

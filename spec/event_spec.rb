c0f275f3b8d3997fedb105b21bd2bfcad044768d
Dylan Perry <dylanbperry@gmail.com>
2023-11-06 08:39:50 -0800

    Add attr_reader for @name and @food_trucks

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -8,4 +8,12 @@ describe Event do
 
         expect(event).to be_a Event
     end
+
+    it "stores and reads a name, and creates default empty food_trucks array" do
+        event = Event.new("South Pearl Street Farmers Market")
+        food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+
+        expect(event.name).to eq("South Pearl Street Farmers Market")
+        expect(event.food_trucks).to eq([])
+    end
 end

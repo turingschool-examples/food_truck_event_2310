062c79f8fd5039400e44c3ad88e7796a5d0bbe94
Yain Porter <yainporter@gmail.com>
2023-11-06 09:47:14 -0700

    feat: Create test and code for #food_trucks_that_sell

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -68,4 +68,29 @@ RSpec.describe Event do
     end
   end
 
+  describe '#food_trucks_that_sell' do
+    event = Event.new("South Pearl Street Farmers Market")
+    food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+    item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    food_truck1.stock(item1, 35)
+    food_truck1.stock(item2, 7)
+    food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+    food_truck2.stock(item4, 50)
+    food_truck2.stock(item3, 25)
+    food_truck3 = FoodTruck.new("Palisade Peach Shack")
+    food_truck3.stock(item1, 65)
+    event.add_food_truck(food_truck1)
+    event.add_food_truck(food_truck2)
+    event.add_food_truck(food_truck3)
+
+    it 'has one parameter of Item and returns an Array of all the FoodTrucks that sell the Item' do
+      expect(event.food_trucks_that_sell(item1)).to be_a Array
+      expect(event.food_trucks_that_sell(item1)).to eq([food_truck1, food_truck3])
+      expect(event.food_trucks_that_sell(item4)).to eq([food_truck2])
+    end
+  end
+
 end

4da1679ba0fa4fc6166a0c0e4ba32fcafb4335d0
Barry Allahyar <barrya@gmail.com>
2023-11-06 12:19:40 -0600

    add inventory and check_stock test

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -18,6 +18,20 @@ RSpec.describe FoodTruck do
     expect(item2.name).to eq("Apple Pie (Slice)")
     expect(item2.price).to eq(2.50)
     expect(food_truck.name).to eq("Rocky Mountain Pies")
+    expect(food_truck.inventory).to eq({})
+  end
+
+  it 'begins with no inventory' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    expect(food_truck.inventory).to eq({})
+  end
+
+  it 'returns 0 if there is no item in inventory' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+
+    expect(food_truck.check_stock(item1)).to eq(0)
   end
 end
     

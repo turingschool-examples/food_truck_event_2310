1188dfb0cade84b66aa5d7fa4281437eea0dee81
Dana Howell <howelld115@gmail.com>
2023-11-06 10:21:40 -0700

    add testing for food_truck #stock, #inventory, and can stock inventory.  incomplete #stock and #check_stock methods in class file

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,8 +1,22 @@
 class FoodTruck
-  attr_reader :name, :inventory
+  attr_reader :name, 
+              :inventory, 
+              :stock
 
   def initialize(name)
     @name = name
     @inventory = {}
+    @stock = 0
   end
+
+  def stock(item, item_quantity)
+    require 'pry' ; binding.pry
+    @inventory << inventory[item] += quantity
+  end
+
+  def check_stock(item)
+    inventory[:item]
+  end
+
+
 end

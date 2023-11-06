c9c25b0091a42a4bf81fe45d5263c93b584bcd4d
Barry Allahyar <barrya@gmail.com>
2023-11-06 12:43:48 -0600

    add test to check if additional item can be stocked

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -39,7 +39,17 @@ RSpec.describe FoodTruck do
     item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
     food_truck.stock(item1, 30)
 
+    expect(food_truck.inventory).to eq({item1 => 30})
     expect(food_truck.check_stock(item1)).to eq(30)
+  end
+
+  it 'can stock additional items' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+    food_truck.stock(item1, 25)
+
+    expect(food_truck.check_stock(item1)).to eq(55)
   end
 end
     

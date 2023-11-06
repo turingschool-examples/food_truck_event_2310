9a8673f1dc0614ee1f1412bbb40bce385953ae6c
Yain Porter <yainporter@gmail.com>
2023-11-06 09:51:58 -0700

    fix: Make sure that the test in Item says that price is a Float, and create test spec for #potential_revenue

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Item do
       expect(Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})).to be_a Item
     end
 
-    it 'has @name that is a String and @price that is an Integer' do
+    it 'has @name that is a String and @price that is a Float' do
       expect(item1.name).to be_a String
       expect(item1.price).to be_a Float
       expect(item1.name).to eq("Peach Pie (Slice)")

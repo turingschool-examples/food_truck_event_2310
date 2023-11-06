a032e50bc081c64ecaae0eeb855be325eb88d867
Logan Finnegan <logantfinnegan@protonmail.com>
2023-11-06 10:25:44 -0600

    initial food truck tests

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -1,2 +1,21 @@
 require './lib/item'
 require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  before(:each) do
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
+  it 'exists' do
+    expect(@food_truck).to be_a(FoodTruck)
+  end
+
+  it 'has readable attributes' do
+    expect(@food_truck.name).to eq("Rocky Mountain Pies")
+    expect(@food_truck.inventory).to eq({})
+    expect(@food_truck.check_stock(item1)).to eq(0)
+  end
+end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,8 +1,8 @@
 require './lib/item'
 RSpec.describe Item do
   before(:each) do
-  @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-  @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
   it 'exists' do

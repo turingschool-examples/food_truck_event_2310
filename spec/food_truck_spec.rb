6f0421dd84fb8884f099682e193e72b107aa59b5
Barry Allahyar <barrya@gmail.com>
2023-11-06 11:39:35 -0600

    add initialize with attributes test to spec file

---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,20 @@
+require './lib/item'
+require './lib/food_truck'
+
+
+RSpec.describe FoodTruck do
+  it 'exists' do
+  
+    expect(food_truck).to be_an_instance_of(FoodTruck)
+  end
+
+  it 'has attributes' do
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    expect(item2.name).to eq("Apple Pie (Slice)")
+    expect(item2.price).to eq(2.50)
+    expect(@food_truck.name).to eq("Rocky Mountain Pies")
+  end
+    

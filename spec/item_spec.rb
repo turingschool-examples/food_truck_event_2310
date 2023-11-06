4310f5a9a792d358d4a0da48e0bffd7c43eae603
Logan Finnegan <logantfinnegan@protonmail.com>
2023-11-06 10:19:21 -0600

    add item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -5,9 +5,13 @@ RSpec.describe Item do
   @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
   end
 
-    it 'exists' do
-      expect(@item1).to be_a(Item)
-      expect(@item2).to eq(Item)
-    end
+  it 'exists' do
+    expect(@item1).to be_a(Item)
+    expect(@item2).to eq(Item)
+  end
+
+  it 'has readable attributes' do
+    expect(@item2.name).to eq("Apple Pie (Slice)")
+    expect(@item2.price).to eq(2.50)
   end
 end

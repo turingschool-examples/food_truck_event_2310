2a1e28833b5c560c6d2d5ad5a1866bebf79a63c4
Martin Chavez <martinchavez1203@gmail.com>
2023-11-06 10:02:04 -0700

    create test for Event class

---
+++ spec/event_spec.rb
@@ -0,0 +1,21 @@
+require './lib/item'
+require './lib/food_truck'
+require './lib/event'
+
+RSpec.describe Event do
+  before(:each) do
+    @event = Event.new("South Pearl Street Farmers Market") 
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@event).to be_a(Event)
+    end
+  end
+
+  describe '#attributes' do
+    it 'reads attrritbutes and returns them' do
+      expect().to eq()
+    end
+  end
+end

d769802797d41fd37a6e235782f8260f46570eb3
Logan Finnegan <logantfinnegan@protonmail.com>
2023-11-06 11:29:26 -0600

    finish it one tests

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -7,16 +7,17 @@ class FoodTruck
               # :check_stock
   def  initialize(name)
     @name = name
-    @inventory = {}
-    # @check_stock = 0
+    @inventory = Hash.new(0)
+    # require 'pry'; binding.pry
   end
 
  def check_stock(food_item)
   @inventory[food_item] || 0
+  # require 'pry'; binding.pry
  end
 
  def stock(food_item, quantity)
-  @inventory[food_item] = quantity
+  @inventory[food_item] += quantity
 
  end
 end
--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -22,7 +22,15 @@ RSpec.describe FoodTruck do
 
   it 'Can stock items' do
     @food_truck.stock(@item1, 30)
-require 'pry'; binding.pry
+# require 'pry'; binding.pry
     expect(@food_truck.check_stock(@item1)).to eq(30)
+
+    @food_truck.stock(@item1, 25)
+
+    expect(@food_truck.check_stock(@item1)).to eq(55)
+    # require 'pry'; binding.pry
+    @food_truck.stock(@item2, 12)
+
+    expect(@food_truck.inventory).to eq({@item1 => 55, @item2 => 12})
   end
 end

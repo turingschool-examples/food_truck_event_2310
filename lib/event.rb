83282f2291565fbfcce38aa989b1eda0120e7d93
Meg Roth <megroth2@gmail.com>
2023-11-06 09:56:43 -0700

    add add_food_truck method to event class and spec

--- lib/event.rb
+++ lib/event.rb
@@ -1,9 +1,17 @@
 class Event
-  attr_reader :name, food_trucks
+  attr_reader :name, :food_trucks
 
   def initialize(name)
     @name = name
     @food_trucks = []
+  end
+
+  # def food_truck_names
+    
+  # end
+
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck if !@food_trucks.include?(food_truck)
   end
 
 end

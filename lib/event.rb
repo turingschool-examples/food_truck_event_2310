4bc7ed8c09ae5ca994106df39bbe147e94a41ad1
Isaac Mitchell <tmitchellisaac@gmail.com>
2023-11-06 09:53:07 -0700

    Add food truck names method

--- lib/event.rb
+++ lib/event.rb
@@ -8,9 +8,16 @@ class Event
     @food_trucks = []
   end
 
-  
 
+  def add_food_truck(food_truck)
+    @food_trucks << food_truck
+  end
 
+  def food_truck_names
+    @food_trucks.map do |truck|
+     truck.name
+    end
+  end
 
 
 

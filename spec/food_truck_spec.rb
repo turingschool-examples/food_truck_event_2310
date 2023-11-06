b7f42dbcea49dc18f5fcbc2f409377b236570bfa
Barry Allahyar <barrya@gmail.com>
2023-11-06 12:34:53 -0600

    add test to see if FoodTruck can stock an item

--- spec/food_truck_spec.rb
+++ spec/food_truck_spec.rb
@@ -33,5 +33,13 @@ RSpec.describe FoodTruck do
 
     expect(food_truck.check_stock(item1)).to eq(0)
   end
+
+  it 'can stock items' do 
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    food_truck.stock(item1, 30)
+
+    expect(food_truck.check_stock(item1)).to eq(30)
+  end
 end
     
